06483d5cd4577c6e34757b238331c065b5d7cbbf
yevgeniy zaslavskiy <yevgeniy.zaslavskiy@gmail.com>
2019-08-10 17:58:18 +0000

    e1

--- client/client-01/entrypoint.sh
+++ client/client-01/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -v http://192.168.0.103:20081 -o "/var/opt/dump/log-$(date +'%Y-%m-%d %H:%M:%S').txt"
+curl -v http://${SERVER} -o "/var/opt/dump/log-$(date +'%Y-%m-%d %H:%M:%S').txt"
 tail -f /dev/null
 #bash
 
